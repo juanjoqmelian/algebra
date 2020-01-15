1754c20c1c260332977eb6199a6fb50c37d7e6f2
juanjoqmelian <juanjosequintana@gmail.com>
2020-01-15 20:32:22 +0000

    Add add, subtract and scalar product operations

--- test/vector_test.py
+++ test/vector_test.py
@@ -13,4 +13,13 @@ class VectorTest(unittest.TestCase):
     def test_add_two_vectors(self):
         vector1 = Vector([1.5, 3.2, 1.6])
         vector2 = Vector([2, 0.8, 1.6])
-        self.assertEqual(vector1.plus(vector2), Vector([3.5, 4.0, 3.2]))
+        self.assertEqual(Vector([3.5, 4.0, 3.2]), vector1.plus(vector2))
+
+    def test_subtract_two_vectors(self):
+        vector1 = Vector([3.2, 2.4, 5.4, 6.7])
+        vector2 = Vector([3.2, 2.2, 1.4, 2.4])
+        self.assertEqual(Vector([0, 0.2, 4.0, 4.3]), vector1.minus(vector2))
+
+    def test_scalar_produce(self):
+        vector = Vector([2.3, 1.1, 3.4, 9.2])
+        self.assertEqual(Vector([4.6, 2.2, 6.8, 18.4]), vector.times_scalar(2))
--- vector.py
+++ vector.py
@@ -1,14 +1,27 @@
+from decimal import Decimal, getcontext
+
+
 class Vector:
     def __init__(self, coordinates) -> None:
         super().__init__()
         self.coordinates = coordinates
         self.dimension = len(coordinates)
+        getcontext().prec = 2
 
     def plus(self, vector):
         return Vector([x + y for x, y in zip(self.coordinates, vector.coordinates)])
 
+    def minus(self, vector):
+        return Vector([float(Decimal(x) - Decimal(y)) for x, y in zip(self.coordinates, vector.coordinates)])
+
+    def times_scalar(self, scalar):
+        return Vector([x * scalar for x in self.coordinates])
+
     def __str__(self) -> str:
-        "Vector : {}".format(self.coordinates)
+        return "Vector : {}".format(self.coordinates)
+
+    def __repr__(self) -> str:
+        return "Vector : {}".format(self.coordinates)
 
     def __eq__(self, o) -> bool:
         return self.coordinates == o.coordinates
